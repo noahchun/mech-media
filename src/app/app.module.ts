984911ed207ebcb6a5a21525f4da51460f0c4bbe
Noah <noahchun77@gmail.com>
2023-10-31 20:15:04 -0400

    started header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
+  // for components
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
+  // for modules
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
